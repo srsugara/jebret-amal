74b95a4ca940b9ab12728e008b78e1f412743a74
srsugara <syauqisugara@gmail.com>
2017-11-07 22:00:32 +0700

    change project structure

--- App.js
+++ App.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Dimensions } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
+let {width, height} = Dimensions.get('window')
 
 export default class App extends React.Component {
+
   render() {
     return (
       <View style={styles.container}>
         <Text>---------------- Jebret Amal ----------------</Text>
-        <Text>Open up App.js to start working on your app!</Text>
         <Text>Hello,You have to increase your amal ok :)</Text>
-        <Entypo name="500px-with-circle" size={32} color="green" />
+        <Entypo name="500px-with-circle" size={50} color="green" />
+        
       </View>
     );
   }
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop:30,
+    paddingBottom:10,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
